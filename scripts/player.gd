1cf632355d086dc6c778940cbde880216b4f1732
Kira Pracht <pracht.kira@outlook.de>
2024-07-16 20:12:18 +0200

    Boundaries refactor

--- scripts/player.gd
+++ scripts/player.gd
@@ -3,7 +3,6 @@ extends CharacterBody2D
 @export var movementSteps : int = 100
 
 func _physics_process(delta):
-	
 	velocity = Vector2(0,0)
 	
 	if Input.is_action_pressed("ui_up"):
@@ -15,15 +14,8 @@ func _physics_process(delta):
 	if Input.is_action_pressed("ui_right"):
 		velocity.x = movementSteps
 	
-	if global_position.x < 0:
-		global_position.x = 0
-	if global_position.x > 420:
-		global_position.x = 420
-	if global_position.y < 50:
-		global_position.y = 50
-	if global_position.y > 650:
-		global_position.y = 650
-		
-	print(global_position)
-	
 	move_and_slide()
+	
+	var screenSize = get_viewport_rect().size
+
+	global_position = global_position.clamp(Vector2(0,0), screenSize)
